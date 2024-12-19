d8ea69bdcd647957535a77abf3217fe6bd43362c
Rawabi <ak@hsoub.com>
2024-12-19 11:51:18 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './assets/sass/styles.scss';
+import 'normalize.css/normalize.css';
+
+console.log("hello from webpack");
+
+var student = require("./studentname");
+var student1 = new student("Noor", "Hilles");
+ 
+sentTimeout( () => alert("hello student!"), 4000);
